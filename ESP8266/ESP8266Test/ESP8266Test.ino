72b93f354e2b42a800b4c55201cb45d2d811e96b
CarbonicBuckey <Kevinmj99@gmail.com>
2020-06-17 03:43:30 -0400

    Test code ESP8266 operational

---
+++ ESP8266/ESP8266Test/ESP8266Test.ino
@@ -0,0 +1,35 @@
+// Code Adapted from https://create.arduino.cc/projecthub/harshmangukiya/create-esp8266-web-server-9c32ac
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+ESP8266WebServer server(80);
+
+const char* ssid = "PrestressedInternet"; //Enter Wi-Fi SSID
+const char* password =  "8AF96Rogers"; //Enter Wi-Fi Password
+ 
+void setup() {
+  Serial.begin(115200); //Begin Serial at 115200 Baud
+  WiFi.begin(ssid, password);  //Connect to the WiFi network
+  
+  while (WiFi.status() != WL_CONNECTED) {  //Wait for connection
+      delay(500);
+      Serial.println("Waiting to connect...");
+  }
+  
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  //Print the local IP
+  
+  server.on("/", handle_index); //Handle Index page
+  
+  server.begin(); //Start the server
+  Serial.println("Server listening");
+}
+
+void loop() {
+  server.handleClient(); //Handling of incoming client requests
+}
+
+void handle_index() {
+  //Print Hello at opening homepage
+  server.send(200, "text/plain", "Hello! This is an index page.");
+}
